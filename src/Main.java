479e595ede618706f3c22cfbd53104b47defdab4
github <mmoloshova@gmail.com>
2022-12-28 21:22:56 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,125 @@
+public class Main {
+    public static void main(String[] args) {
+//        String result =method(5);
+//        if(result=="true") {
+//            System.out.println("jup");
+//        } else{
+//                System.out.println("tak");
+//            }
+//
+//    }
+//      static String method(int a){
+//        if((a*a)%2==0){
+//            return "true";
+//        }else{
+//            return"false";
+//          }
+
+/**2index**/
+//        int[] arr = new int[]{2, 3, 4, 5, 7, 9};
+//        System.out.println(getIndex(arr, 8));
+//    }
+//    static int getIndex(int[] array, int a) {
+//        for (int i = 0; i < array.length; i++) {
+//            if (array[i] >= a) {
+//                return i;
+//            }
+//        }
+//        return array.length;
+//    }
+//}
+        /**index**/
+//        System.out.println(array1(new int[]{1,2,3,4,5,6,10}));
+//    }
+//
+//    static int array1(int[] array) {
+//        Scanner sc = new Scanner(System.in);
+//        int b = sc.nextInt();
+//        for (int i = 0; i < array.length; i++) {  // индекс табуу
+//            if (b <= array[i] ) {
+//                return i;
+//            }
+//        }
+//        return array.length;
+
+
+   /*     if(san(6)==Math.pow(4,2)){
+            System.out.println("eko biri birine barabar ");
+        }else{
+            System.out.println("eko biri birine barabar emes");
+        }
+    }
+    static int san(int a){
+        return a+a;*/
+
+
+//        System.out.println(number());
+//    }
+//
+//    static int number() {
+//        int num1 = 0;
+//        for (int i = 0; i <300; i++) {
+//            if (i % 3 == 0 && i % 4 == 0 && i % 5 == 0) {
+//                System.out.println(i);
+//                num1 += i;
+//            }
+//        }
+//        return num1;
+//
+//    }
+
+
+/**varargs**/
+//       method(5,5,6,7,3,4,5,55,5,9) ;
+//    }
+//    static void method(int ...a){
+//        for (int i = 0; i < a.length ; i++) {
+//            System.out.println(a[i]);
+//
+//        }
+
+//        System.out.println(method(5,12,34,3,55,15));
+//    }
+//     static int method(int...arr) {
+//         int summ = 0;
+//         for (int i = 0; i <arr.length; i++) {
+//             summ+=arr[i];
+//         }
+//       return summ;
+        //        int[]array={3,4,5,6,7,8,9};
+//        for(int a: array) {
+//         if(a%2==0){
+//            System.out.println(a);
+//        }
+//    }
+
+
+//        String[] array={"Madina,Nurisa,Nurida,Guli,Rustam"};
+//        for (String a: array){
+//            System.out.println(a+"I love you ");
+//        }
+
+      /**string method**/
+
+    String word ="java";
+        System.out.println(word.charAt(2));
+        System.out.println(word.substring(2,4));
+        System.out.println(word.contains("JAVA"));
+        System.out.println(word.toUpperCase());
+
+
+
+//     Scanner scan=new Scanner(System.in);
+//     method(scan.nextLine());
+//    }
+//     static String method(String soz){
+//         System.out.println(soz.toUpperCase());
+//         System.out.println(soz.trim());
+//         System.out.println(soz.contains("A"));
+//
+
+
+     }
+}
+
+
